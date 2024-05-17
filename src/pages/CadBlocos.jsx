bd03657159cbfc9608fac49d0b0e1ee6f559d323
Christian <chritiangabriel4@gmail.com>
2024-05-17 20:36:07 -0300

    cadBlocos completa

--- src/pages/CadBlocos.jsx
+++ src/pages/CadBlocos.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
 import LayoutCad from "../components/LayoutCad";
+import '../css/CadBlocos.css'
+import { faBuildingColumns, faCube, faCubes } from "@fortawesome/free-solid-svg-icons";
 
 const CadBlocos = () => {
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleSelectChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+
+    const handleChange = () => {
+        
+    };
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <LayoutCad>
-            
+        <LayoutCad title='Cadastro de Bloco.' icon={faCube}>
+            <form className='Ajustes'>
+                <div>
+                    <label className='titulo-inputs-bloco'>Polo</label>
+                    <select className='select-bloco' id="" name="Polo" value={selectedOption} onChange={handleSelectChange}>
+                        <option value="Polo">Integrado Centro</option>
+                    </select>
+                </div>
+                <div>
+                    <label className='titulo-inputs-bloco'>Nome do Bloco</label>
+                    <div  className="grid">
+                        <input
+                            className='input-bloco'
+                            type="text"
+                            name="cd_cpfcnpj"
+                            // value={colocar}
+                            onChange={handleChange}
+                            required
+                        />
+                        <button className='adiciona-polo' type="submit" onSubmit={handleSubmit}>Adicionar</button>
+                    </div>
+                </div>
+                <button className='botao' type="submit" onSubmit={handleSubmit}>Cadastro de algo</button>
+            </form>
         </LayoutCad>
     );
 }
