23a45ceed77fb1867d44231f5b3fed7d5c8d4139
PedroFrancholi <francholidearaujo@gmail.com>
2024-05-20 20:54:42 -0300

    Cad

--- src/pages/CadBlocos.jsx
+++ src/pages/CadBlocos.jsx
@@ -4,8 +4,40 @@ import '../css/CadBlocos.css'
 import {  faCube } from "@fortawesome/free-solid-svg-icons";
 
 const CadBlocos = () => {
+    const { isAuthenticated } = useAuth();
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
 
     const [selectedOption, setSelectedOption] = useState('');
+    const [nm_bloco, setNm_bloco] = useState('');
+    const [polos, setPolos] = useState([]);
+
+    const buscarPolo = useCallback(async () => {
+        try {
+          const response = await axios.get("http://localhost:8080/consulta-polo", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          const data = response.data.instituicoes;
+          if (Array.isArray(data)) {
+            setPolos(data);
+          } else {
+            console.error("Formato inexperado do respose de polo", data);
+            setPolos([]);
+          }
+        } catch (error) {
+          console.log(error.response.data);
+        }
+      }, [token]);
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+          navigate("/login");
+        } else {
+          buscarPolo();
+        }
+      }, [isAuthenticated, navigate, buscarInstituicao, buscarMunicipio]);
 
     const handleSelectChange = (event) => {
       setSelectedOption(event.target.value);
