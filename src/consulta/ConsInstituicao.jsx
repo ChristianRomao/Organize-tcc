b945813ce36814564a9ded3e7dc5efc1216704b8
Christian <chritiangabriel4@gmail.com>
2024-05-28 23:53:52 -0300

    tela de consulta base feita, layout de consulta completo tambem

--- src/consulta/ConsInstituicao.jsx
+++ src/consulta/ConsInstituicao.jsx
@@ -1,10 +1,26 @@
+import { faBuildingColumns } from "@fortawesome/free-solid-svg-icons";
 import LayoutConsulta from "../components/LayoutConsulta";
-import '../css/LayoutConsulta.css';
+import '../css/ConsInstituicao.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ConsInstituicao = () => {
     return (
         <LayoutConsulta titleCons='Consulta Instituição'>
-
+            <div className="informacoes-inst">
+                <FontAwesomeIcon icon={faBuildingColumns} size="3x" style={{color: "#001636"}}/>
+                <div className="consulta-instituicao">
+                    <h4>Razão social:</h4>
+                    <p>Centro Universitário Integrado</p>
+                </div>
+                <div className="consulta-instituicao">
+                    <h4>CNPJ:</h4>
+                    <p>1234567890009</p>
+                </div>
+                <div className="consulta-instituicao">
+                    <h4>Nome Fantasia:</h4>
+                    <p>Integrado</p>
+                </div>
+            </div>
         </LayoutConsulta>
     );
 }
