6a18b80d6c9a4262c923b585e9df27646bf1800e
Christian <chritiangabriel4@gmail.com>
2024-06-08 18:03:31 -0300

    fiz na master sem querer '-'

--- src/consulta/ConsUsuario.jsx
+++ src/consulta/ConsUsuario.jsx
@@ -1,19 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LayoutConsulta from "../components/LayoutConsulta";
+import '../consCss/ConsUsuario.css';
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const ConsUsuario = () => {
     return (
         <LayoutConsulta titleCons='Consulta Usuários'>
-            <div className="consulta-grade">
-                <h4>Disciplina:</h4>
-                <p>Java</p>
-            </div>
-            <div className="consulta-grade">
-                <h4>Turma:</h4>
-                <p>4º Periodo</p>
-            </div>
-            <div className="consulta-grade">
-                <h4>Ano Letivo:</h4>
-                <p>2023</p>
+            <div className="informacoes-usuario">
+            <FontAwesomeIcon icon={faUser} size="3x" style={{color: "#001636"}}/>
+                <div className="consulta-usuario">
+                    <h4>Usuário:</h4>
+                    <p>Christian</p>
+                </div>
+                <div className="consulta-usuario">
+                    <h4>Email:</h4>
+                    <p>chritiangabriel4@gmail.com</p>
+                </div>
+                <div className="consulta-usuario">
+                    <h4>Função:</h4>
+                    <p>Administrador</p>
+                </div>
             </div>
         </LayoutConsulta>
     );
