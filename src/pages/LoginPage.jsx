f9f7264ec0fc7281676b8d489db7fca78ae7a826
PedroFrancholi <francholidearaujo@gmail.com>
2024-06-13 23:01:12 -0300

    feat/login_page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -112,20 +112,20 @@ const LoginPage = () => {
                     <div className='sobre'>
                         <h1 className='titulo-sobre'>Quem somos?</h1>
                         <div>
-                            <h4>Missão:</h4>
+                            <h4 className='subtitle'>Missão:</h4>
                             <p className='content-sobre'>
                                 Facilitar a gestão acadêmica por meio de soluções inovadoras e eficientes, proporcionando uma organização otimizada das salas de aula e recursos materiais, para melhorar a experiência acadêmica de todos os envolvidos.
                             </p>
-                            <h4>Visão:</h4>
+                            <h4 className='subtitle'>Visão:</h4>
                             <p className='content-sobre'>
                                 Ser reconhecida como referência em tecnologia educacional, oferecendo ferramentas que impulsionam a eficiência e a transparência na gestão acadêmica, contribuindo para o sucesso das instituições de ensino.
                             </p>
-                            <h4>Valores:</h4>
+                            <h4 className='subtitle'>Valores:</h4>
                             <ul>
-                                <li>
-                                    <strong>Inovação:</strong> Buscamos constantemente soluções criativas e tecnologicamente avançadas para atender às necessidades em constante evolução do setor educacional.
+                                <li className='content-sobre'>
+                                    <strong>Inovação:</strong> Buscamos constantemente soluções criativas e tecnologicamente avançadas para atender às necessidades em constante evolução do setor  educacional.
                                 </li>
-                                <li>
+                                <li className='content-sobre'>
                                     <strong>Eficiência:</strong> Comprometemo-nos em desenvolver sistemas que otimizam processos e facilitam a rotina dos usuários, proporcionando maior produtividade e satisfação.
                                 </li>
                             </ul>
