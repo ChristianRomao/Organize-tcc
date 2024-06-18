b802dda53ac62b36b2eb69d8ed2b3af50dcf93d0
Christian <chritiangabriel4@gmail.com>
2024-06-18 10:03:04 -0300

    ajuste de css da reserva

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -78,13 +78,13 @@ const HomePage = () => {
                             <p className='titulos-home'>Consulta</p>
                             <div className='btn-gerais scrollable'>
                                 {/* <button className='btn' type="button" onClick={() => handleConsulta('instituicao')}>Instituição</button> */}
-                                <button className='btn' type="button" onClick={() => handleConsulta('polo')}>Polo</button>
                                 <button className='btn' type="button" onClick={() => handleConsulta('bloco')}>Bloco</button>
-                                <button className='btn' type="button" onClick={() => handleConsulta('sala')}>Sala</button>
                                 <button className='btn' type="button" onClick={() => handleConsulta('material')}>Materiais</button>
+                                <button className='btn' type="button" onClick={() => handleConsulta('polo')}>Polo</button>
+                                <button className='btn' type="button" onClick={() => handleConsulta('sala')}>Sala</button>
+                                <button className='btn' type="button" onClick={() => handleConsulta('grade')}>Turmas</button>
                                 <button className='btn' type="button" onClick={() => handleConsulta('usuario')}>Usuários</button>
                                 {/* <button className='btn' type="button" onClick={() => handleConsulta('turma')}>Turma</button> */}
-                                <button className='btn' type="button" onClick={() => handleConsulta('grade')}>Turmas</button>
                             </div>
                         </div>
                     </div>
